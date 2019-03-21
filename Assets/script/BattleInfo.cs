ac038e9817cfc6d843a4a6bda9fff11b3c854b00
alex86106pig <alex86106pig@gmail.com>
2019-03-21 17:58:15 +0800

    Develop

--- Assets/script/BattleInfo.cs
+++ Assets/script/BattleInfo.cs
@@ -10,21 +10,57 @@ public class BattleInfo {
 
     public class CharacterInbattle
     {
-        public GameObject characterObj;
-        public float Timer = 0;
-        public float Hp;
-        public float Ag;
-        public float At;
-        public bool Dead = false;
+        GameObject characterObj;
+        float timer;
+        float hp;
+        float ag;
+        float at;
+        bool dead;
+        public void Set(GameObject characterObj, float Timer, float Hp, float Ag, float At)
+        {
+            this.CharacterObj = characterObj;
+            this.Timer = Timer;
+            this.Hp = Hp;
+            this.Ag = Ag;
+            this.At = At;
+            this.Dead = false;
+        }
+        public GameObject CharacterObj
+        {
+            set { characterObj = value; }
+            get { return characterObj; }
+        }
+        public float Timer
+        {
+            set { timer = value; }
+            get { return timer; }
+        }
+        public float Hp
+        {
+            set { hp = value; }
+            get { return hp; }
+        }
+        public float Ag
+        {
+            set { ag = value; }
+            get { return ag; }
+        }
+        public float At
+        {
+            set { at = value; }
+            get { return at; }
+        }
+        public bool Dead
+        {
+            set { dead = value; }
+            get { return dead; }
+        }
     }
 
     public void SetCharacterInbattle(GameObject characterObj, float Timer, float Hp, float Ag, float At)
     {
         CharacterInbattle buffer = new CharacterInbattle();
-        buffer.characterObj = characterObj;
-        buffer.Hp = Hp;
-        buffer.Ag = Ag;
-        buffer.At = At;
+        buffer.Set(characterObj, Timer, Hp, Ag, At);
         characterInbattle.Add(buffer);
     }
 
